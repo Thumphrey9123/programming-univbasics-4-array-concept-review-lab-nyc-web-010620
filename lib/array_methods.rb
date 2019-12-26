22965fc89d8c35ea6d0d26d0f9c57ae14d65d447
Thomas Humphrey <thumphrey9123@gmail.com>
2019-12-26 23:21:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -24,7 +24,7 @@ def find_min_value(array)
   small = []  
   counter = 0 
   while array.length>counter do
-    if array[counter]>small
+    if array[counter]<small
       small = array[counter]
     end
     counter+=1 
